0cc7ff9fa9d282b4d5dc5af2c03de13a6b0e30ce
Robin Sharma <sharms115@gmail.com>
2016-10-18 04:13:50 -0400

    nav components

--- app/router.js
+++ app/router.js
@@ -2,17 +2,18 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('bidness');
-
-  this.route('admin', function() {
-    this.route('invitations');
-  });
+    this.route('about');
+    this.route('bidness');
+    this.route('admin', function() {
+        this.route('invitations');
+    });
+    this.route('libraries', function() {
+        this.route('new');
+    });
 });
 
 export default Router;
